1c8db9789bbf54d2ade8829d917a0ff82fa9b2d8
Eduardo Souza <eduardoa.ms14@gmail.com>
2023-12-23 12:58:29 -0300

    primeiro commit - criação do projeto

---
+++ src/test/java/plano/saude/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package plano.saude.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

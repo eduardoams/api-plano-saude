16ff3c27b2cd61f138e5c93df94cf871af1a7062
Eduardo Souza <eduardoa.ms14@gmail.com>
2023-12-28 08:38:59 -0300

    feat: implementação da busca por id e ativos

--- src/main/java/plano/saude/api/repository/BeneficiarioRepository.java
+++ src/main/java/plano/saude/api/repository/BeneficiarioRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import plano.saude.api.domain.Beneficiario;
+import plano.saude.api.domain.dto.beneficiario.ListingBeneficiarioDTO;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BeneficiarioRepository extends JpaRepository<Beneficiario, Long> {
 
     Page<Beneficiario> findAllByAtivoTrue(Pageable p);
+    Optional<Beneficiario> findByIdAndAtivoTrue(Long id);
 }
